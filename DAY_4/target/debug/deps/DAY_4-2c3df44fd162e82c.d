f693ef8dfb6dde56f099cd0919b5a4e3e31f05e4
62Xenon <khsuhan100@gmail.com>
2025-06-05 18:50:20 +0545

    ADDING DAY_5 FILE

---
+++ DAY_4/target/debug/deps/DAY_4-2c3df44fd162e82c.d
@@ -0,0 +1,9 @@
+/home/xlegion/RUST/ISPP60days-Challenge/DAY_4/target/debug/deps/libDAY_4-2c3df44fd162e82c.rmeta: src/main.rs Cargo.toml
+
+/home/xlegion/RUST/ISPP60days-Challenge/DAY_4/target/debug/deps/DAY_4-2c3df44fd162e82c.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
---
+++ Day_5/target/debug/Day_5.d
@@ -0,0 +1,1 @@
+/home/xlegion/RUST/ISPP60days-Challenge/Day_5/target/debug/Day_5: /home/xlegion/RUST/ISPP60days-Challenge/Day_5/src/main.rs
