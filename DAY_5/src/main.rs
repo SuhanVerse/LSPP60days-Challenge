86d4812b85bae4ec782f2c87715688dacd0cff09
62Xenon <khsuhan100@gmail.com>
2025-06-05 18:59:41 +0545

    1ST PROG OF DAY _5

--- DAY_5/src/main.rs
+++ DAY_5/src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
-}
+// #![allow(dead_code)]
+// #![allow(unused_variables)]
+// enum Direction {
+//     North,
+//     South,
+//     East,
+//     West,
+// }
+
+// fn main() {
+//     let dir1 = Direction::North;
+//     let dir2 = Direction::East;
+
+//     // Using a simple match
+//     match dir1 {
+//         Direction::North => println!("Going north"),
+//         Direction::South => println!("Going south"),
+//         Direction::East  => println!("Going east"),
+//         Direction::West  => println!("Going west"),
+//     }
+
+//     // Using a match with a guard
+//     match dir2 {
+//         Direction::North => println!("Still going north"),
+//         Direction::South => println!("Now going south"),
+//         Direction::East  => {
+//             if let Direction::East = dir2 {
+//                 println!("Heading east");
+//             }
+//         },
+//         Direction::West  => println!("Turning west"),
+//     }
+
+// }
+
