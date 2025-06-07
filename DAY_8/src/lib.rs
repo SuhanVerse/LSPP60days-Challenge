c4b14585fe69de2df369b4e69d6e2107a18b8497
62Xenon <khsuhan100@gmail.com>
2025-06-07 20:36:55 +0545

    1ST COMMIT OF DAY_8

---
+++ DAY_8/src/lib.rs
@@ -0,0 +1,8 @@
+// src/lib.rs
+
+// Export the `utils` and `math` modules so they’re visible from the crate root
+pub mod math;
+pub mod utils;
+
+// Re-export utils::greet so users of this library can call `day8_project::greet`
+pub use utils::greet;
