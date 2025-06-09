58a685672676ef81dddd308717805c5fac8d7fd1
62Xenon <khsuhan100@gmail.com>
2025-06-09 18:20:06 +0545

    add files of DAY_9 and math.rs

---
+++ DAY_9/target/debug/deps/DAY_9-61d6be08e8b18474.d
@@ -0,0 +1,5 @@
+/home/xlegion/RUST/ISPP60days-Challenge/DAY_9/target/debug/deps/DAY_9-61d6be08e8b18474: src/main.rs
+
+/home/xlegion/RUST/ISPP60days-Challenge/DAY_9/target/debug/deps/DAY_9-61d6be08e8b18474.d: src/main.rs
+
+src/main.rs:
