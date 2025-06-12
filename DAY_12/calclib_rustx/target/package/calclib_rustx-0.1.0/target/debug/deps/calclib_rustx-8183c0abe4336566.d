e489d17a8de9028666e24f3ccf216c167def0e98
62Xenon <khsuhan100@gmail.com>
2025-06-12 21:08:38 +0545

    Renamed to xrust_calclib

--- DAY_12/calclib_rustx/target/package/calclib_rustx-0.1.0/target/debug/deps/calclib_rustx-8183c0abe4336566.d
+++
@@ -1,7 +0,0 @@
-/home/xlegion/RUST/ISPP60days-Challenge/DAY_12/calclib_rustx/target/package/calclib_rustx-0.1.0/target/debug/deps/libcalclib_rustx-8183c0abe4336566.rmeta: src/lib.rs
-
-/home/xlegion/RUST/ISPP60days-Challenge/DAY_12/calclib_rustx/target/package/calclib_rustx-0.1.0/target/debug/deps/libcalclib_rustx-8183c0abe4336566.rlib: src/lib.rs
-
-/home/xlegion/RUST/ISPP60days-Challenge/DAY_12/calclib_rustx/target/package/calclib_rustx-0.1.0/target/debug/deps/calclib_rustx-8183c0abe4336566.d: src/lib.rs
-
-src/lib.rs:
--- DAY_12/calclib_rustx/target/package/calclib_rustx-0.1.0/target/debug/deps/calclib_rustx-de27ee8a72ab7079.d
+++
@@ -1,5 +0,0 @@
-/home/xlegion/RUST/ISPP60days-Challenge/DAY_12/calclib_rustx/target/debug/deps/calclib_rustx-de27ee8a72ab7079: src/lib.rs
-
-/home/xlegion/RUST/ISPP60days-Challenge/DAY_12/calclib_rustx/target/debug/deps/calclib_rustx-de27ee8a72ab7079.d: src/lib.rs
-
-src/lib.rs:
