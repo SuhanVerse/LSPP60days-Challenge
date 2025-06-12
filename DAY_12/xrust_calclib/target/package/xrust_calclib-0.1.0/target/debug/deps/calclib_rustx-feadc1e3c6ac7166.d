d4695e78d439a87af5a9419946cdb17758177678
62Xenon <khsuhan100@gmail.com>
2025-06-12 21:20:01 +0545

    dry run test

--- DAY_12/xrust_calclib/target/package/xrust_calclib-0.1.0/target/debug/deps/calclib_rustx-feadc1e3c6ac7166.d
+++
@@ -1,7 +0,0 @@
-/home/xlegion/RUST/ISPP60days-Challenge/DAY_12/calclib_rustx/target/debug/deps/libcalclib_rustx-feadc1e3c6ac7166.rmeta: src/lib.rs
-
-/home/xlegion/RUST/ISPP60days-Challenge/DAY_12/calclib_rustx/target/debug/deps/libcalclib_rustx-feadc1e3c6ac7166.rlib: src/lib.rs
-
-/home/xlegion/RUST/ISPP60days-Challenge/DAY_12/calclib_rustx/target/debug/deps/calclib_rustx-feadc1e3c6ac7166.d: src/lib.rs
-
-src/lib.rs:
--- DAY_12/xrust_calclib/target/package/xrust_calclib-0.1.0/target/debug/deps/xrust_calclib-4d8d0911785801dd.d
+++
@@ -1,5 +0,0 @@
-/home/xlegion/RUST/ISPP60days-Challenge/DAY_12/xrust_calclib/target/debug/deps/xrust_calclib-4d8d0911785801dd: src/lib.rs
-
-/home/xlegion/RUST/ISPP60days-Challenge/DAY_12/xrust_calclib/target/debug/deps/xrust_calclib-4d8d0911785801dd.d: src/lib.rs
-
-src/lib.rs:
