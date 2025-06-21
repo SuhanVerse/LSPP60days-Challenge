fc08d63d187658c06c3bec318162064ea1fdce65
62Xenon <khsuhan100@gmail.com>
2025-06-21 17:42:10 +0545

    ADD FILES OF DAY_21

---
+++ DAY_21/src/main.rs
@@ -0,0 +1,23 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+use cortex_m_rt::entry;
+use cortex_m_semihosting::{debug, hprintln};
+
+#[entry]
+fn main() -> ! {
+    // Print a debug message via semihosting
+    hprintln!("Starting no_std debug demo");
+    // Exit with success code (stops QEMU semihost session)
+    debug::exit(debug::EXIT_SUCCESS);
+    // NOTE: this point is never reached, but the function must diverge
+    loop {}
+}
+
+// Required when you do not pull in panic_semihosting:
+// Define a minimal panic handler that just spins
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
